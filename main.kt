e00c8378ed079bbfc77f4f22faf9100378bd57df
yeonuk44 <yeonuk44@gmail.com>
2023-07-19 14:43:56 +0900

    # Add function

--- main.kt
+++ main.kt
@@ -8,4 +8,17 @@ fun main(args: Array<String>) {
     age = 58
     println(age)
 
+    printStudentInfo("yeonuk1", 10)
+    printStudentInfo("yeonuk2", 20)
+    printStudentInfo("yeonuk3", 30)
+
+    println(addNum(100,100))
+}
+fun printStudentInfo(name: String, age: Int) : Unit{
+    println("student name : " + name)
+    println("student age : " + age)
+}
+
+fun addNum(n1: Int, n2:Int) : Int {
+    return n1 + n2
 }
